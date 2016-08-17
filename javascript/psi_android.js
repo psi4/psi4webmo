ff00f2560c8a697b1f9e6f5d41d3b91d4a9c7e86
JR Schmidt <jrschmidt2@gmail.com>
2016-08-17 15:25:58 -0500

    Updating PSI scripts for WebMO v17 compatability

---
+++ javascript/psi_android.js
@@ -0,0 +1,26 @@
+function SubmitJob(form,preview)
+{
+	moledit.setModelFromBuilder();
+	var response =JSON.parse(moledit.getProperties());
+		
+	document.form.dimScanned.value = response.dimScanned;
+	document.form.scanVar.value = response.varScanned;
+	document.form.scanStart.value = response.varScanned2;
+	document.form.scanStop.value = response.scanStop;
+	document.form.scanSteps.value = response.scanSteps;
+	document.form.scanVar2.value = response.dimScanned;
+	document.form.scanStart2.value = response.scanStart2;
+	document.form.scanStop2.value = response.scanStop2;
+	document.form.scanSteps2.value = response.scanSteps2;
+	
+	if (form.cartesianCoordinates.checked) getGeometry("XYZFormat", "", form.geometry, false);
+	else getGeometry("PSI4Format", "writeEqualsSign=true", form.geometry, false);
+	
+	getGeometry("XYZFormat", "writeUnitCell=true", form.cartesian, false);
+	getGeometry("ConnectionFormat", "", form.connections, false);
+	getGeometry("ZMatrixFormat", "", form.zmatrix, false);
+	getGeometry("ChargeFormat", "", form.charges, false);
+	
+	DoSubmitJob(form, preview);		
+}
+		
---
+++ javascript/psi_ios.js
@@ -0,0 +1,34 @@
+function SubmitJob(form,preview)
+{
+	bridge.send("setModelFromBuilder");
+	bridge.send("getProperties",
+
+	function (responseData) {
+		var response = JSON.parse(responseData);
+		
+		document.form.dimScanned.value = response.dimScanned;
+		document.form.scanVar.value = response.varScanned;
+		document.form.scanStart.value = response.varScanned2;
+		document.form.scanStop.value = response.scanStop;
+		document.form.scanSteps.value = response.scanSteps;
+		document.form.scanVar2.value = response.dimScanned;
+		document.form.scanStart2.value = response.scanStart2;
+		document.form.scanStop2.value = response.scanStop2;
+		document.form.scanSteps2.value = response.scanSteps2;
+	});
+
+	if (form.cartesianCoordinates.checked) getGeometry("XYZFormat", "", form.geometry, false);
+	else getGeometry("PSI4Format", "writeEqualsSign=true", form.geometry, false);
+	
+	getGeometry("XYZFormat", "writeUnitCell=true", form.cartesian, false);
+	getGeometry("ConnectionFormat", "", form.connections, false);
+	getGeometry("ZMatrixFormat", "", form.zmatrix, false);
+	getGeometry("ChargeFormat", "", form.charges, false);
+	
+	bridge.send("DONE",
+
+	function (responseData) {
+		DoSubmitJob(form, preview);
+	});		
+}
+		
